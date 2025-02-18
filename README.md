9faaeabec3bf1d220012d38cc8e90f6b80f5b443
goatsefl <amt@DESKTOP-PNQVTDT>
2025-02-18 16:55:08 +0530

    Added : Directory - FLutter tools

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FlutterLearning
 Learning flutter to create faster cross-platform app.
+Added: roll_dice [Project Based Learning].
+
---
+++ roll_dice/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart'; // Main package for flutter, which can use 
+
+//pubspec.yaml contains all our dependencies which is later used to 
+
+
+void main(){ // main() is where the main part of the code is run.
+  runApp();// runApp takes in the parameter which is used to run our app.
+}
